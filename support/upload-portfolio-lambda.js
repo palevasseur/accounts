efaf7ab062f9abe8b96d13bd049c273ea5563e3c
Paul-Antoine <palevasseur@gmail.com>
2019-08-04 13:34:46 +0200

    use pipeline src zip build

--- support/upload-portfolio-lambda.js
+++ support/upload-portfolio-lambda.js
@@ -1,18 +1,22 @@
 const aws = require('aws-sdk');
 const AdmZip = require('adm-zip');
 
-const bucket = 'accountsapp-build.cg-palevasseur75';
-const key = 'accountsapp.zip';
-const codepipeline = new aws.CodePipeline({apiVersion: '2015-07-09'})
+const codepipeline = new aws.CodePipeline({apiVersion: '2015-07-09'});
+const bucketDest = 'accountsapp-build.cg-palevasseur'; // public bucket to deploy index.html & sources
 
 exports.handler = async function(event, context) {
-  const jobId = event["CodePipeline.job"].id;
+  const job = event["CodePipeline.job"];
+  const jobId = job ? job.id : undefined;
+  const buildZip = job ? job.data.inputArtifacts[0].location.s3Location.objectKey : 'accountsapp.zip'; // default value for test
+  const bucketSrc = job ? job.data.inputArtifacts[0].location.s3Location.bucketName : bucketDest; // default value same as dest for test
+  console.log('bucket src=' + bucketSrc + ', build zip=' + buildZip + ', bucket dest=' + bucketDest);
+
   try {
     // get zip file
     const s3 = new aws.S3({ apiVersion: '2006-03-01' });
     const inputFile = await s3.getObject({
-      Bucket: bucket,
-      Key: key,
+      Bucket: bucketSrc,
+      Key: buildZip,
     }).promise();
 
     // unzip content in the same
@@ -20,28 +24,29 @@ exports.handler = async function(event, context) {
     const entries = inputZip.getEntries();
     let bucketsList = [];
     entries.forEach((entry) => {
-      console.log('Found entry ' + entry.entryName);
       const params = {
-        Bucket: bucket,
+        Bucket: bucketDest,
         Key: entry.entryName,
         Body: entry.getData(),
         ContentType: 'content-type'
       };
-      console.log('Creating file, params=', params);
+      console.log('Creating file "' + entry.entryName + '" with params=', params);
       bucketsList.push(s3.putObject(params).promise());
     });
 
     // wait all promise before end
     await Promise.all(bucketsList);
 
-    // propagate pipeline result
-    await codepipeline.putJobSuccessResult({jobId}).promise();
+    if(jobId) {
+      // propagate pipeline result
+      await codepipeline.putJobSuccessResult({jobId}).promise();
+    }
   }
   catch (err) {
-    /*console.log(err);
-    const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+    console.log(err);
+    const message = `Error getting object ${buildZip} from bucket ${bucketSrc}. Make sure they exist and your bucket is in the same region as this function.`;
     console.log(message);
-    throw new Error(message);*/
+
     const params = {
       jobId,
       failureDetails: {
